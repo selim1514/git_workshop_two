11a235325027945216933e2650d8c86234776952
gokburak <johnfiber10@gmail.com>
2021-11-23 20:51:29 +0300

    değişiklik

--- leap-year.py
+++ leap-year.py
@@ -3,7 +3,7 @@
 year = int(input("Enter a year: "))
 
 if (year % 4) == 0:
-    if (year % 100) == 0:
+    if (year % 100) != 0:
         if (year % 400) == 0:
             print("{0} is a leap year".format(year))
         else:
@@ -11,4 +11,8 @@ if (year % 4) == 0:
     else:
         print("{0} is a leap year".format(year))
 else:
+
     print("{0} is not a leap year".format(year))
+
+
+!
