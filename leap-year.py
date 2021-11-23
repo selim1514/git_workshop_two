0366993e987de063fa619e37f981b3e12072e087
mustafa <mustafa@Seralp.localdomain>
2021-11-23 21:16:32 +0300

    leap year değişlik yapıldı

--- leap-year.py
+++ leap-year.py
@@ -3,7 +3,7 @@
 year = int(input("Enter a year: "))
 
 if (year % 4) == 0:
-    if (year % 100) != 0:
+    if (year % 100) 6= 0:
         if (year % 400) == 0:
             print("{0} is a leap year".format(year))
         else:
